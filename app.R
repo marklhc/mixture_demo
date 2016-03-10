2a140ffbf164e1f9f5c61e0d08429f1059fd5cda
marklhc <mark.lai@uc.edu>
2016-03-10 00:50:38 -0500

    Add panel below title for explaining the app. Add checkbox for constraining the SDs to be equal.

--- app.R
+++ app.R
@@ -3,10 +3,28 @@ library(mixtools)
 
 ui <- fluidPage(
   titlePanel("Simple Demonstration of Univariate Normal Mixture"),
+  fluidRow(
+    column(width = 12, 
+           wellPanel(
+             p("In this app, there are two variables from two different data
+               sets."), 
+             p("You may specify the number of clusters and click the 
+               'Iterate!' button to see how the EM algorithm proceeds."),
+             p("The tables below the distribution showed the estimated
+               mixing proportion as well as the means and SDs for each 
+               latent class after each iterations. The colored solid lines 
+               represent the normal curve for each class, and the dashed black
+               line represents the kernel density estimates."), 
+             p("Try running the EM alogrithm multiple times, and you will
+               find that sometimes the algorithm will converge to different
+               solutions!")
+           ))
+  ), 
   sidebarLayout(
     sidebarPanel(
       sliderInput("n_clus", label = "Number of clusters:",
                   min = 2, max = 4, value = 2, step = 1, ticks = FALSE), 
+      checkboxInput("sd_equal", "Equal SDs across classes", value = FALSE), 
       selectInput("n_iter", "Number of iterations", list(1, 10, 100)),
       actionButton("iter", "Iterate!"), 
       br(), 
@@ -51,6 +69,8 @@ server <- function(input, output) {
       data(CO2data)
       CO2data$CO2
     }
+    if (input$sd_equal) sd_constr <- rep("a", k)
+    else sd_constr <- NULL
     isolate({
       if (v$initialized == FALSE) {
         v$lambda0 <- runif(k)
@@ -61,8 +81,9 @@ server <- function(input, output) {
       } else {
         n_iter <- as.numeric(input$n_iter)
       }
+      print(sd_constr)
       mixmdl <- normalmixEM(y, v$lambda0, v$mu0, v$sigma0, 
-                            maxit = n_iter)
+                            maxit = n_iter, sd.constr = sd_constr)
       v$lambda0 <- mixmdl$lambda
       v$mu0 <- mixmdl$mu
       v$sigma0 <- mixmdl$sigma
